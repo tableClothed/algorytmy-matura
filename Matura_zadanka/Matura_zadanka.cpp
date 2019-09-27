03065f27d292b7c5fb673665a3abc821356413ce
Weronika Żak <weronikarzak@gmail.com>
2019-09-27 23:56:34 +0200

    dwojkowy, osemkowy

--- Matura_zadanka/Matura_zadanka.cpp
+++ Matura_zadanka/Matura_zadanka.cpp
@@ -3,10 +3,75 @@
 
 #include "pch.h"
 #include <iostream>
+#include <string.h>
+#include <math.h>
+
+using namespace std;
+
+void Anagramy(string slowo1) {
+	string slowo2 = slowo1;
+	bool czy_anagram = true;
+	for (int i = 0; i < slowo1.length(); i++) {
+		if (slowo1[i] == slowo2[slowo1.length() - i]) {
+			czy_anagram = false;
+			break;
+		}
+	}
+	cout << "Czy annagram: " << czy_anagram << endl;
+}
+
+void Dwojkowy(int liczba) {
+	string binarna = "";
+	while (liczba > 0) {
+		if (liczba % 2 == 0) {
+			binarna += "0";
+		}
+		else {
+			binarna += "1";
+		}
+		liczba = round(liczba/2);
+	}
+
+
+	for (int i = binarna.length() - 1; i >= 0; i--)
+	{
+		cout << binarna[i];
+	}
+	cout << endl;
+
+}
+
+void Osemkowy(int liczba) {
+	int potegi = -1;
+	int kopia1 = liczba;
+
+	while (kopia1 > 0) {
+		kopia1 /= 8;
+		potegi++;
+	}
+
+	cout << "Potegi " << potegi << endl;
+
+	while (liczba > 0) {
+		int modulo = pow(8, potegi);
+
+		cout << liczba / modulo;
+
+		liczba = liczba % modulo;
+		//cout << "Liczba " << liczba << " modulo " << modulo << endl;
+		potegi--;
+	}
+	cout << endl;
+
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+
+	//Anagramy("asddsa");
+	//Dwojkowy(20);
+	Osemkowy(8);
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
