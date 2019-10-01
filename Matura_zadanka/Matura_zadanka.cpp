c4a8e9ae9cd1861ae310cb858bdc736050e2a932
Weronika Żak <weronikarzak@gmail.com>
2019-10-01 23:24:17 +0200

    MIN MAX optymalne, sortowanie przez wstawianie

--- Matura_zadanka/Matura_zadanka.cpp
+++ Matura_zadanka/Matura_zadanka.cpp
@@ -1,7 +1,4 @@
-﻿// Matura_zadanka.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
 #include <string.h>
 #include <math.h>
@@ -168,7 +165,8 @@ int Euklides_Rekurencja(int a, int b) {
 	
 }
 
-void Najwiekszy_Najmniejszy(int tablica[]) {
+void Najwiekszy_Najmniejszy() {
+	int tablica[] = { 5, 6, 2, 23, 1, 0, -3, -5, 0 };
 	int najmniejszy = 99999;
 	int najwiekszy = -99999;
 	for (int i = 0; i < 9; i++) {
@@ -178,7 +176,6 @@ void Najwiekszy_Najmniejszy(int tablica[]) {
 
 	cout << "Najwiekszy " << najwiekszy << ", najmniejszy " << najmniejszy;
 }
-
 
 void Wyswietl_tablice(int tablica[], int wielkosc) {
 	for (int i = 0; i < wielkosc; i++) {
@@ -200,15 +197,108 @@ void Sortowanie_Babelkowe() {
 				tablica[i] = tablica[i - 1];
 				tablica[i - 1] = temp;
 			}
-
 		}
 		a--;
 	}
-	
 
 	Wyswietl_tablice(tablica, 7);
 
+}
 
+void Sortowanie_przez_selekcje() {
+	int tablica[] = { 1, 44, 23, 100, 0, -5, 9 };
+	int a = 7;
+
+	Wyswietl_tablice(tablica, a);
+
+	int index = 0;
+	
+	for (int i = 0; i < a; i++) {
+		index = i;
+		for (int j = i + 1; j < a; j++) {
+			if (tablica[j] < tablica[index]) {
+				index = j;
+			}
+
+		}
+		//Wyswietl_tablice(tablica, a);
+
+		swap(tablica[i], tablica[index]);
+
+	}
+
+	Wyswietl_tablice(tablica, a);
+
+}
+
+void Sortowanie_Wstawianie() {
+	int tablica[] = { 1, 44, 23, 100, 0, -5, 9 };
+	int a = 7;
+
+	Wyswietl_tablice(tablica, a);
+
+	int pozycja_poczatkowa = 1;
+	int i = pozycja_poczatkowa;
+
+	int liczba, index;
+
+	for (int i = 0; i < a; i++) {
+		liczba = tablica[i];
+		index = i - 1;
+
+		while (index >= 0 && tablica[index] > liczba) {
+			tablica[index + 1] = tablica[index];
+			index--;
+		}
+		tablica[index + 1] = liczba;
+	}
+
+	Wyswietl_tablice(tablica, a);
+}
+
+void Min_Max_Optymalne() {
+	int tablica[] = { 1, 44, 23, 100, -5, 0, 9 };
+	int a = 7;
+
+	Wyswietl_tablice(tablica, a);
+
+	int MIN, MAX;
+
+	MIN = tablica[0] > tablica[1] ? tablica[1] : tablica[0];
+	MAX = tablica[0] > tablica[1] ? tablica[0] : tablica[1];
+
+	int x = 2;
+
+	while (x + 2 <= a) {
+		if (tablica[x] > tablica[x - 1]) {
+			MAX = tablica[x] > MAX ? tablica[x] : MAX;
+			MIN = tablica[x-1] < MIN ? tablica[x-1] : MIN;
+		}
+		else {
+			MAX = tablica[x - 1] > MAX ? tablica[x-1] : MAX;
+			MIN = tablica[x] < MIN ? tablica[x] : MIN;
+		}
+		x += 2;
+	}
+
+	if (a % 2 != 0) {
+		MAX = tablica[a - 1] > MAX ? tablica[a - 1] : MAX;
+		MIN = tablica[a - 1] < MIN ? tablica[a - 1] : MIN;
+	}
+
+	cout << "MIN " << MIN << " MAX " << MAX;
+}
+
+void Sortowanie_Skalanie() {
+	int tablica1[] = { 1, 44, 23, 100, -5, 0, 9 };
+	int a1 = 7;
+
+	Wyswietl_tablice(tablica1, a1);
+
+	int tablica2[] = { 555, 55, -23, -10, 5, 0, -23 };
+	int a2 = 7;
+
+	Wyswietl_tablice(tablica2, a2);
 }
 
 
@@ -226,7 +316,10 @@ int main()
 	//cout << Rekurencyjna_Fibonacci(13);
 	//Euklides(24, 2341);
 	//cout << Euklides_Rekurencja(920, 920);
-	//int tablica[] = { 5, 6, 2, 23, 1, 0, -3, -5, 0}; Najwiekszy_Najmniejszy(tablica);
-	Sortowanie_Babelkowe();
-	
+	//Najwiekszy_Najmniejszy();
+	//Sortowanie_Babelkowe();
+	//Sortowanie_przez_selekcje();
+	//Sortowanie_Wstawianie();
+	//Min_Max_Optymalne();
+	Sortowanie_Skalanie();
 }
