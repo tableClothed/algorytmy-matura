5a5f7fab32bf2345877c40d244561a56f66605e0
Weronika Żak <weronikarzak@gmail.com>
2019-10-03 20:44:44 +0200

    matura 2014/2015

--- Matura_zadanka/Matura_zadanka.cpp
+++ Matura_zadanka/Matura_zadanka.cpp
@@ -289,17 +289,151 @@ void Min_Max_Optymalne() {
 	cout << "MIN " << MIN << " MAX " << MAX;
 }
 
-void Sortowanie_Skalanie() {
-	int tablica1[] = { 1, 44, 23, 100, -5, 0, 9 };
-	int a1 = 7;
+void Scal_Tablice(int tablica[], int lewy, int srodek, int prawy) {
+	int i = lewy;
+	int j = srodek + 1;
 
-	Wyswietl_tablice(tablica1, a1);
+	int pomocna_tablica[7];
 
-	int tablica2[] = { 555, 55, -23, -10, 5, 0, -23 };
-	int a2 = 7;
+	for (int i = lewy; i <= prawy; i++) {
+		pomocna_tablica[i] = tablica[i];
+	}
 
-	Wyswietl_tablice(tablica2, a2);
+	for (int k = lewy; k <= prawy; k++) {
+		if (i <= srodek) {
+			if (j <= prawy) {
+				if (pomocna_tablica[j] < pomocna_tablica[i])
+					tablica[k] = pomocna_tablica[j++];
+				else
+					tablica[k] = pomocna_tablica[i++];
+			}
+			else {
+				tablica[k] = pomocna_tablica[i++];
+			}
+		}
+		else {
+			tablica[k] = pomocna_tablica[j++];
+		}
+	}
+
+	Wyswietl_tablice(tablica, 7);
 }
+
+
+void Sortowanie_Skalanie(int tablica[], int lewy, int prawy) {
+
+
+	//int tablica2[] = { 555, 55, -23, -10, 5, 0, -23 };
+
+	//Wyswietl_tablice(tablica2, a);
+
+	int srodek = (lewy + prawy) / 2;
+
+	Sortowanie_Skalanie(tablica, lewy, srodek);
+	Sortowanie_Skalanie(tablica, srodek+1, prawy);
+
+	Scal_Tablice(tablica, lewy, srodek, prawy);
+}
+
+void Sort_Scal() {
+	int a = 7;
+
+	int tablica1[] = { 1, 44, 23, 100, -5, 0, 9 };
+
+	Wyswietl_tablice(tablica1, a);
+
+	Sortowanie_Skalanie(tablica1, 0, a - 1);
+}
+
+void Sortowanie_Szybkie(int tablica[], int lewy, int prawy) {
+
+	int i = lewy - 1;
+	int j = prawy + 1;
+
+	int pivot = tablica[(lewy + prawy) / 2];
+
+	while (true) {
+		while (pivot > tablica[++i]) {
+			while (pivot < tablica[--j])
+			{
+				if (i <= j)
+					swap(tablica[i], tablica[j]);
+				else
+					break;
+			}
+		}
+
+		if (j > lewy)
+			Sortowanie_Szybkie(tablica, lewy, j);
+		else
+			Sortowanie_Szybkie(tablica, i, prawy);
+
+	}
+
+}
+
+void Quick_Sort() {
+	int a = 7;
+	int tablica[] = { 1, 44, 23, 100, -5, 0, 9 };
+
+	Sortowanie_Szybkie(tablica, 0, a - 1);
+}
+
+void Liczba_Progow() {
+	int tablica[] = { 2, 2, 2, 3, 1, 1, 3, 3, 1, 10, 11, 7, 7, 6, 7, 7, 8, 9, 9, 7 };
+	int n = 20;
+	int i = 1;
+	int liczba_prog = 0;
+
+
+	while (i < n) {
+		if (tablica[i - 1] >= tablica[i])
+		{
+			if (tablica[i - 1] != tablica[i]) {
+				liczba_prog++;
+				cout << tablica[i - 1] << " " << tablica[i] << endl;
+			}
+
+		}
+		i++;
+	}
+
+	cout << liczba_prog;
+
+}
+
+
+void Najwieksza_Liczba_Progow() {
+	//int tablica[] = { 2, 2, 2, 3, 1, 1, 3, 3, 1, 10, 11, 7, 7, 6, 7, 7, 8, 9, 9, 7 };
+	int tablica[] = { 20, 19, 18, 17, 16, 15, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+	int n = 20;
+	int i = 1;
+	int liczba_prog = 0;
+	int najw_liczb = 0;
+
+
+	while (i <= n) {
+		if ((tablica[i - 1] >= tablica[i]) && (i != n))
+		{
+			if (tablica[i - 1] != tablica[i]) {
+				liczba_prog++;
+				//cout << tablica[i - 1] << " " << tablica[i] << endl;
+			}
+
+		} else {
+			if (liczba_prog > najw_liczb) {
+				najw_liczb = liczba_prog;
+			}
+			liczba_prog = 0;
+
+		}
+		i++;
+	}
+
+	cout << najw_liczb;
+
+}
+
 
 
 
@@ -321,5 +455,8 @@ int main()
 	//Sortowanie_przez_selekcje();
 	//Sortowanie_Wstawianie();
 	//Min_Max_Optymalne();
-	Sortowanie_Skalanie();
+	//Sort_Scal();
+	//Quick_Sort();
+	//Liczba_Progow();
+	Najwieksza_Liczba_Progow();
 }
